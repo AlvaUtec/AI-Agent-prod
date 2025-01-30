9a4b737b4013352e5e8c9ce9aa42c5e25a7abf58
AlvaUtec <alvaro.garcia.h@utec.edu.pe>
2025-01-29 23:08:57 -0500

    Dadjoke

--- src/agent.ts
+++ src/agent.ts
@@ -14,24 +14,26 @@ export const runAgent = async ({
   await addMessages([{ role: 'user', content: userMessage }])
   
   const loader = showLoader('🤔')
+  while (true){
   const history = await getMessages()
   
   const response = await runLLM({ messages: history, tools })
   await addMessages([response])
+
+  if (response.content) {
+    loader.stop()
+    logMessage(response)
+    return getMessages()
+  }
   
   if (response.tool_calls) {
     const toolCall = response.tool_calls[0]
-
+    logMessage(response)
     loader.update(`🛠️ ${toolCall.function.name}`)
 
     const toolResponse = await runTool(toolCall, userMessage)
     await saveToolResponse(toolCall.id, toolResponse)
     loader.update(`done: ${toolCall.function.name}`)
-
+    }
   }
-
-
-  logMessage(response)
-  loader.stop()
-  return getMessages()
 }
